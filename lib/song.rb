5cf020b3d6c1309491e0f68671fe8f8db76bb4fa
Jamal Turner <jlsmitty2273@yahoo.com>
2019-10-03 14:04:17 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,24 @@
+class Song
+  attr_accessor :name, :artist, :genre
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    #@@artists
+    #@@genres
+  end
+
+  def count
+    Song.count
+  end
+  #def self.all?
+  #  @@artists << self
+  #  @@genres << self
+  #end
+
+end
